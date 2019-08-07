d34fef8e3a306abddd9bccc920eb0716e4d799d2
Ваше Имя <you@example.com>
2019-08-07 14:19:35 +0300

    standard commit

--- src/source.py
+++ src/source.py
@@ -5,7 +5,12 @@ app=Flask(__name__)
 
 @app.route('/',methods=['post'])
 def echo():
-    response=request
-    response.response.text=str(request)
+    response ={
+        'version': request.json['version'],
+        'session': request.json['session'],
+        'response':{
+            'text':request
+        }
+    }
     return response
 app.run(host='0.0.0.0',port=os.getenv('PORT',5000))
