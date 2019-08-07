8ae9a46cc810d6bd5a3f8a8bfc336cffc52fe2df
Ваше Имя <you@example.com>
2019-08-07 15:04:54 +0300

    standard commit

--- src/source.py
+++ src/source.py
@@ -3,14 +3,17 @@ import os
 
 app=Flask(__name__)
 
+players=[]
+
 @app.route('/',methods=['post'])
 def echo():
-    response ={
-        'version': request.json['version'],
-        'session': request.json['session'],
-        'response':{
-            'text':request.json
+    if request.json['original_utterance'] == "":
+        response ={
+            'version': request.json['version'],
+            'session': request.json['session'],
+            'response':{
+                'text':str(request.json[text][::-1]==request.json[text])
+            }
         }
-    }
     return response
 app.run(host='0.0.0.0',port=os.getenv('PORT',5000))
